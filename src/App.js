81d31126082babc61e7599ab0b1da519e26f8256
Chibuzo Franklin Odigbo <cfaodigbo@gmail.com>
2023-02-23 13:15:52 +0100

    added page loader, slight changes

--- src/App.js
+++ src/App.js
@@ -2,11 +2,13 @@ import './App.scss';
 import { ThemeToggleProvider } from './components/Theme/ThemeToggle';
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import Landing from './pages/Landingpage/Landing';
+import PageLoader from './pages/PageLoader';
 
 function App() {
   return (
     <ThemeToggleProvider>
       <div className="App">
+      <PageLoader />
         <Router>
           <Routes>
             <Route path='/' element={<Landing/>} />
