d4d56ae22cd37abed64d01d678ec704b2120c377
Chibuzo Franklin Odigbo <cfaodigbo@gmail.com>
2023-02-26 18:55:55 +0100

    small colour changes

--- src/components/Services/Services.js
+++ src/components/Services/Services.js
@@ -2,27 +2,37 @@ import React, {useContext } from 'react'
 import './Services.scss'
 import Service from './service.json'
 import mobile from'../../assets/mobile.png'
+import apple from'../../assets/appleStore.svg'
+import play from'../../assets/playStore.svg'
 import { ThemeToggleContext } from '../Theme/ThemeToggle'
 
 const Services = () => { 
     const { theme } = useContext(ThemeToggleContext)
 
   return (
-    <section className={`services ${theme}`}>
-        <img src={mobile} alt="" title='Mo  bile phone' />
+    <section className={`service ${theme}`}>
+        <h1>Features</h1>
+        <section className={`services`}>
+            <img src={mobile} alt="" title='Mobile phone' />
 
-        <aside>
-            {
-                Service && Service.map( service => {
-                    return(
-                        <div className="box" key={ service.id }>
-                            <i className={service.icon}></i>
-                            <p>{service.title}</p>
-                        </div>
-                    )
-                })
-            }
-        </aside>
+            <aside>
+                {
+                    Service && Service.map( service => {
+                        return(
+                            <div className="box" key={ service.id }>
+                                <i className={service.icon}></i>
+                                <p>{service.title}</p>
+                            </div>
+                        )
+                    })
+                }
+            </aside>
+        </section>
+        <div className="buttons">
+            <button type="submit">More Features</button>
+            <a href=""><img src={apple} alt="" /></a> 
+            <a href=""><img src={play} alt="" /> </a>
+        </div>
     </section>
   )
 }
